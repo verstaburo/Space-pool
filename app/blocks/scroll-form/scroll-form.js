792c7082899a03538a07d327e193292a0fa55eaf
S5477 <utrobinslavik@gmail.com>
2018-12-06 18:01:32 +0500

    sticky header

--- app/blocks/scroll-form/scroll-form.js
+++ app/blocks/scroll-form/scroll-form.js
@@ -3,5 +3,8 @@ import 'sticky-kit/dist/sticky-kit';
 const $ = window.$;
 
 export default function scrollCont() {
-  $('.scroll-form').stick_in_parent();
+  $('.scroll-form').stick_in_parent({ offset_top: 120 });
+  $('.header').stick_in_parent().on('sticky_kit:stick', () => {
+    $('.header').addClass('header-js');
+  });
 }
