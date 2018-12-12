1dd1c979e276d36da3d11c97358e61de2edcd786
S5477 <utrobinslavik@gmail.com>
2018-12-12 17:23:40 +0500

    fixx

--- app/blocks/accordion/accordion.js
+++ app/blocks/accordion/accordion.js
@@ -1,49 +1,19 @@
-export const HIDDEN = 'accordion/shown';
-export const SHOWN = 'accordion/hidden';
-export const BEFORE_SHOW = 'accordion/beforeshow';
-export const BEFORE_HIDE = 'accordion/beforehide';
-
-const DURATION = 250;
 
 const $ = window.$;
 
-$(document).on('click', '.js-accordion-button', function (e) {
+$(document).on('click', '.js-accordion-button', (e) => {
   e.preventDefault();
-  const button = $(this);
-  const block = button.parents('.accordion');
-  const body = block.find('.accordion__body');
-  const isActive = Number(block.hasClass('is-active'));
-  const isMultiple = block.parents('.accordions').data('accordion-multiple');
+  const block = e.target.parentElement.parentElement;
+  const acc = $('.accordion');
+  const accBtn = $('.accordion__button');
 
-  if (block.hasClass('is-disabled')) {
-    return;
-  }
-
-  const beforeEvent = [BEFORE_SHOW, BEFORE_HIDE][isActive];
-  const afterEvent = [SHOWN, HIDDEN][isActive];
-
-  body.trigger(beforeEvent).slideToggle(DURATION, () => {
-    block
-      .toggleClass('is-active')
-      .trigger(afterEvent);
-    button
-      .toggleClass('is-active-button')
-      .trigger(afterEvent);
-  });
-
-  if (!isMultiple) {
-    const siblings = block.siblings('.accordion.is-active');
-
-    siblings
-      .trigger(BEFORE_HIDE)
-      .find('.accordion__body')
-      .slideUp(DURATION, () => {
-        siblings
-          .removeClass('is-active')
-          .trigger(HIDDEN);
-      });
-    siblings
-      .find('.accordion__button')
-      .removeClass('is-active-button');
+  if (block.classList.contains('is-active-body')) {
+    acc.removeClass('is-active-body');
+    accBtn.removeClass('is-active-button');
+  } else {
+    acc.removeClass('is-active-body');
+    accBtn.removeClass('is-active-button');
+    block.classList.add('is-active-body');
+    e.target.classList.add('is-active-button');
   }
 });
