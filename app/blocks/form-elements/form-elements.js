5877083af23e18c1b64cb0c9e5bdf3b0394e8cf6
Allkrain <aukra25@yandex.ru>
2018-12-26 16:48:52 +0300

    fixes

--- app/blocks/form-elements/form-elements.js
+++ app/blocks/form-elements/form-elements.js
@@ -18,6 +18,62 @@ export function selects() {
     const choices = new Choices('.js-select', {
       searchEnabled: false,
       itemSelectText: '',
+      classNames: {
+        containerOuter: 'choices',
+      },
+      callbackOnCreateTemplates(template) {
+        const classNames = this.config.classNames;
+        return {
+          containerInner: () => template(`
+            <div class="${classNames.containerInner}"><div class="choices__toggle"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16.31 9.16"><line x1="15.31" y1="1" x2="8.16" y2="8.16"/><path d="M8.16,9.16a1,1,0,0,1-.71-.3,1,1,0,0,1,0-1.41L14.61.29A1,1,0,0,1,16,.29a1,1,0,0,1,0,1.42L8.86,8.86A1,1,0,0,1,8.16,9.16Z"/><line x1="8.16" y1="8.16" x2="1" y2="1"/><path d="M8.16,9.16a1,1,0,0,1-.71-.3L.29,1.71A1,1,0,0,1,1.71.29L8.86,7.45a1,1,0,0,1,0,1.41A1,1,0,0,1,8.16,9.16Z"/></svg></div></div>
+          `),
+          dropdown: () => template(`
+            <div class="${classNames.list} ${classNames.listDropdown} js-scrollbar-dark" aria-expanded="false"></div>
+          `),
+        };
+      },
+    });
+  }
+
+  if ($('.js-select-light').length) {
+    const choices = new Choices('.js-select-light', {
+      searchEnabled: false,
+      itemSelectText: '',
+      classNames: {
+        containerOuter: 'choices choices_light',
+      },
+      callbackOnCreateTemplates(template) {
+        const classNames = this.config.classNames;
+        return {
+          containerInner: () => template(`
+            <div class="${classNames.containerInner}"><div class="choices__toggle"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16.31 9.16"><line x1="15.31" y1="1" x2="8.16" y2="8.16"/><path d="M8.16,9.16a1,1,0,0,1-.71-.3,1,1,0,0,1,0-1.41L14.61.29A1,1,0,0,1,16,.29a1,1,0,0,1,0,1.42L8.86,8.86A1,1,0,0,1,8.16,9.16Z"/><line x1="8.16" y1="8.16" x2="1" y2="1"/><path d="M8.16,9.16a1,1,0,0,1-.71-.3L.29,1.71A1,1,0,0,1,1.71.29L8.86,7.45a1,1,0,0,1,0,1.41A1,1,0,0,1,8.16,9.16Z"/></svg></div></div>
+          `),
+          dropdown: () => template(`
+            <div class="${classNames.list} ${classNames.listDropdown} js-scrollbar-dark" aria-expanded="false"></div>
+          `),
+        };
+      },
+    });
+  }
+
+  if ($('.js-select-dark').length) {
+    const choices = new Choices('.js-select-dark', {
+      searchEnabled: false,
+      itemSelectText: '',
+      classNames: {
+        containerOuter: 'choices choices_dark',
+      },
+      callbackOnCreateTemplates(template) {
+        const classNames = this.config.classNames;
+        return {
+          containerInner: () => template(`
+            <div class="${classNames.containerInner}"><div class="choices__toggle"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16.31 9.16"><line x1="15.31" y1="1" x2="8.16" y2="8.16"/><path d="M8.16,9.16a1,1,0,0,1-.71-.3,1,1,0,0,1,0-1.41L14.61.29A1,1,0,0,1,16,.29a1,1,0,0,1,0,1.42L8.86,8.86A1,1,0,0,1,8.16,9.16Z"/><line x1="8.16" y1="8.16" x2="1" y2="1"/><path d="M8.16,9.16a1,1,0,0,1-.71-.3L.29,1.71A1,1,0,0,1,1.71.29L8.86,7.45a1,1,0,0,1,0,1.41A1,1,0,0,1,8.16,9.16Z"/></svg></div></div>
+          `),
+          dropdown: () => template(`
+            <div class="${classNames.list} ${classNames.listDropdown} js-scrollbar-light" aria-expanded="false"></div>
+          `),
+        };
+      },
     });
   }
   /* eslint-enable no-unused-vars */
