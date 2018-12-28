6e67cd3d9d62cdc3a979cd378a57b1892fc1135b
Allkrain <aukra25@yandex.ru>
2018-12-28 11:52:22 +0300

    fix assign problem in ie

--- app/blocks/map/map.js
+++ app/blocks/map/map.js
@@ -13,10 +13,12 @@ export function maps() {
   const map = L.map('map').setView([59.934, 30.335], 13);
 
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    maxZoom: 12,
+    maxZoom: 18,
     id: 'mapbox.streets',
     accessToken: 'pk.eyJ1IjoidGhldmVydmVyeTEiLCJhIjoiY2lzZXdzaXZ4MDBjaTJudm93dDI4MGVrMCJ9.Z8KKk0M_lpDTPB6_JtJBxg',
   }).addTo(map);
+
+  map.zoomControl.setPosition('bottomright');
 
   const marker = L.marker([59.934, 30.335]).addTo(map);
 
@@ -34,29 +36,67 @@ export function mapsTwo() {
   });
 
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    maxZoom: 12,
+    maxZoom: 18,
     id: 'mapbox.streets',
     accessToken: 'pk.eyJ1IjoidGhldmVydmVyeTEiLCJhIjoiY2lzZXdzaXZ4MDBjaTJudm93dDI4MGVrMCJ9.Z8KKk0M_lpDTPB6_JtJBxg',
   }).addTo(mapTwo);
 
-  const marker = L.marker([59.934, 30.315], { icon: myMarker }).addTo(mapTwo);
-  const markerTwo = L.marker([59.9345, 30.3156], { icon: myMarker }).addTo(mapTwo);
-  const markerQ = L.marker([59.924, 30.335], { icon: myMarker }).addTo(mapTwo);
-  const markerW = L.marker([59.924, 30.305], { icon: myMarker }).addTo(mapTwo);
-  const markerE = L.marker([59.924, 30.335], { icon: myMarker }).addTo(mapTwo);
-  const markerR = L.marker([59.914, 30.335], { icon: myMarker }).addTo(mapTwo);
-  const markerT = L.marker([59.934, 30.325], { icon: myMarker }).addTo(mapTwo);
-  const markerA = L.marker([59.900, 30.315], { icon: myMarker }).addTo(mapTwo);
-  const markerS = L.marker([59.934, 30.300], { icon: myMarker }).addTo(mapTwo);
-  const markerD = L.marker([59.913, 30.330], { icon: myMarker }).addTo(mapTwo);
-  const markerF = L.marker([59.927, 30.338], { icon: myMarker }).addTo(mapTwo);
-  const markerG = L.marker([59.948, 30.34], { icon: myMarker }).addTo(mapTwo);
-  const markerH = L.marker([59.988, 30.324], { icon: myMarker }).addTo(mapTwo);
-  const markerJ = L.marker([59.9, 30.34476], { icon: myMarker }).addTo(mapTwo);
-  const markerK = L.marker([59.939, 30.76], { icon: myMarker }).addTo(mapTwo);
-  const markerL = L.marker([59.941, 30.9], { icon: myMarker }).addTo(mapTwo);
-  const markerZ = L.marker([59.966, 30.8], { icon: myMarker }).addTo(mapTwo);
-  const markerX = L.marker([59.977, 30.73735], { icon: myMarker }).addTo(mapTwo);
+  mapTwo.zoomControl.setPosition('bottomright');
+
+  const marker = L.marker([59.934, 30.315], {
+    icon: myMarker,
+  }).addTo(mapTwo);
+  const markerTwo = L.marker([59.9345, 30.3156], {
+    icon: myMarker,
+  }).addTo(mapTwo);
+  const markerQ = L.marker([59.924, 30.335], {
+    icon: myMarker,
+  }).addTo(mapTwo);
+  const markerW = L.marker([59.924, 30.305], {
+    icon: myMarker,
+  }).addTo(mapTwo);
+  const markerE = L.marker([59.924, 30.335], {
+    icon: myMarker,
+  }).addTo(mapTwo);
+  const markerR = L.marker([59.914, 30.335], {
+    icon: myMarker,
+  }).addTo(mapTwo);
+  const markerT = L.marker([59.934, 30.325], {
+    icon: myMarker,
+  }).addTo(mapTwo);
+  const markerA = L.marker([59.900, 30.315], {
+    icon: myMarker,
+  }).addTo(mapTwo);
+  const markerS = L.marker([59.934, 30.300], {
+    icon: myMarker,
+  }).addTo(mapTwo);
+  const markerD = L.marker([59.913, 30.330], {
+    icon: myMarker,
+  }).addTo(mapTwo);
+  const markerF = L.marker([59.927, 30.338], {
+    icon: myMarker,
+  }).addTo(mapTwo);
+  const markerG = L.marker([59.948, 30.34], {
+    icon: myMarker,
+  }).addTo(mapTwo);
+  const markerH = L.marker([59.988, 30.324], {
+    icon: myMarker,
+  }).addTo(mapTwo);
+  const markerJ = L.marker([59.9, 30.34476], {
+    icon: myMarker,
+  }).addTo(mapTwo);
+  const markerK = L.marker([59.939, 30.76], {
+    icon: myMarker,
+  }).addTo(mapTwo);
+  const markerL = L.marker([59.941, 30.9], {
+    icon: myMarker,
+  }).addTo(mapTwo);
+  const markerZ = L.marker([59.966, 30.8], {
+    icon: myMarker,
+  }).addTo(mapTwo);
+  const markerX = L.marker([59.977, 30.73735], {
+    icon: myMarker,
+  }).addTo(mapTwo);
 
 
   marker.bindPopup('Кастомный попап');
