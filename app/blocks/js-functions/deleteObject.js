ae801eb4d854f9f78845fefe6dfeffdad72e5c2a
Allkrain <aukra25@yandex.ru>
2019-01-29 13:56:08 +0300

    popup style

--- app/blocks/js-functions/deleteObject.js
+++ app/blocks/js-functions/deleteObject.js
@@ -28,4 +28,32 @@ export default function deleteObject() {
         },
       });
   });
+
+  $(document).on('click', '.js-delete-object-except-last', (evt) => {
+    evt.preventDefault();
+    const self = $(evt.target).closest('[data-deleted-object]');
+    const group = $(self).parents('[data-deleted-group]');
+    const totalElements = $(group).find('[data-deleted-object]').length;
+    if (totalElements > 1) {
+      const timeline = anime.timeline({
+        easing: 'easeInQuad',
+        duration: 300,
+      });
+      timeline
+        .add({
+          targets: self[0],
+          opacity: 0,
+        })
+        .add({
+          targets: self[0],
+          height: 0,
+          complete() {
+            $(self).remove();
+          },
+        });
+    } else {
+      $(self).find('input').val('');
+      $(self).trigger('input');
+    }
+  });
 }
