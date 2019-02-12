fe79d504c4e7a929a1245df75a3e302d8b138477
Allkrain <aukra25@yandex.ru>
2019-02-12 16:48:54 +0300

    sign up end message

--- app/blocks/sidebar/sidebar.js
+++ app/blocks/sidebar/sidebar.js
@@ -3,7 +3,29 @@ import 'sticky-kit/dist/sticky-kit';
 const $ = window.$;
 
 export default function scrollCont() {
-  $('.js-sticky-block').stick_in_parent({
-    offset_top: 80,
-  });
+  function stickElement(el) {
+    $(el).stick_in_parent({
+      offset_top: 80,
+    });
+  }
+
+  function activationStick() {
+    $('.js-sticky-block').each((i, el) => {
+      if ($(el).attr('data-unstick-media') !== undefined) {
+        const mediaStopper = window.globalOptions.sizes[$(el).attr('data-unstick-media')];
+        console.log(`${mediaStopper}px`);
+        if ($(window).width() < mediaStopper) {
+          $(el).trigger('sticky_kit:detach');
+        } else {
+          stickElement(el);
+        }
+      } else {
+        stickElement(el);
+      }
+    });
+  }
+
+  activationStick();
+
+  $(window).on('resize', activationStick);
 }
