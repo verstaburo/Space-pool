b3452dc9423d00326a8850981d4f910bb9278937
Allkrain <aukra25@yandex.ru>
2019-02-19 08:20:10 +0300

    footer and header special version

--- app/blocks/form-box/form-box.js
+++ app/blocks/form-box/form-box.js
@@ -21,12 +21,36 @@ export default function floatingLabel() {
 
   preset();
 
-  $(document).on('input focus foucsin blur', '.form-box input, .form-box textarea', (evt) => {
-    const self = evt.target;
-    if ($(self).siblings('input').length === 0) {
-      $(self).closest('.form-box').toggleClass('is-top', (evt.type === 'focus' || self.value.length > 0));
-    } else if ($(self).siblings('input')[0].value.length === 0) {
-      $(self).closest('.form-box').toggleClass('is-top', (evt.type === 'focus' || self.value.length > 0));
-    }
+  const targets = document.querySelectorAll('.form-box input, .form-box textarea');
+  targets.forEach((item) => {
+    item.addEventListener('animationstart', (evt) => {
+      console.log('animationstart');
+      const self = evt.target;
+      $(self).closest('.form-box').addClass('is-top');
+    }, true);
+    item.addEventListener('focus', (evt) => {
+      const self = evt.target;
+      if ($(self).siblings('input').length === 0) {
+        $(self).closest('.form-box').toggleClass('is-top', (evt.type === 'focus' || self.value.length > 0));
+      } else if ($(self).siblings('input')[0].value.length === 0) {
+        $(self).closest('.form-box').toggleClass('is-top', (evt.type === 'focus' || self.value.length > 0));
+      }
+    }, true);
+    item.addEventListener('blur', (evt) => {
+      const self = evt.target;
+      if ($(self).siblings('input').length === 0) {
+        $(self).closest('.form-box').toggleClass('is-top', (evt.type === 'focus' || self.value.length > 0));
+      } else if ($(self).siblings('input')[0].value.length === 0) {
+        $(self).closest('.form-box').toggleClass('is-top', (evt.type === 'focus' || self.value.length > 0));
+      }
+    }, true);
+    item.addEventListener('input', (evt) => {
+      const self = evt.target;
+      if ($(self).siblings('input').length === 0) {
+        $(self).closest('.form-box').toggleClass('is-top', (evt.type === 'focus' || self.value.length > 0));
+      } else if ($(self).siblings('input')[0].value.length === 0) {
+        $(self).closest('.form-box').toggleClass('is-top', (evt.type === 'focus' || self.value.length > 0));
+      }
+    }, true);
   });
 }
