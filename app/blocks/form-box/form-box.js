6e193c50f863b2c6f8f542423c637a8a30367a20
Allkrain <aukra25@yandex.ru>
2019-03-14 09:12:56 +0300

    add window.setLabelListeners

--- app/blocks/form-box/form-box.js
+++ app/blocks/form-box/form-box.js
@@ -21,36 +21,42 @@ export default function floatingLabel() {
 
   preset();
 
-  const targets = document.querySelectorAll('.form-box input, .form-box textarea');
-  for (let i = 0; i < targets.length; i += 1) {
-    const item = targets[i];
-    item.addEventListener('animationstart', (evt) => {
-      const self = evt.target;
-      $(self).closest('.form-box').addClass('is-top');
-    }, true);
-    item.addEventListener('focus', (evt) => {
-      const self = evt.target;
-      if ($(self).siblings('input').length === 0) {
-        $(self).closest('.form-box').toggleClass('is-top', (evt.type === 'focus' || self.value.length > 0));
-      } else if ($(self).siblings('input')[0].value.length === 0) {
-        $(self).closest('.form-box').toggleClass('is-top', (evt.type === 'focus' || self.value.length > 0));
-      }
-    }, true);
-    item.addEventListener('blur', (evt) => {
-      const self = evt.target;
-      if ($(self).siblings('input').length === 0) {
-        $(self).closest('.form-box').toggleClass('is-top', (evt.type === 'focus' || self.value.length > 0));
-      } else if ($(self).siblings('input')[0].value.length === 0) {
-        $(self).closest('.form-box').toggleClass('is-top', (evt.type === 'focus' || self.value.length > 0));
-      }
-    }, true);
-    item.addEventListener('input', (evt) => {
-      const self = evt.target;
-      if ($(self).siblings('input').length === 0) {
-        $(self).closest('.form-box').toggleClass('is-top', (evt.type === 'focus' || self.value.length > 0));
-      } else if ($(self).siblings('input')[0].value.length === 0) {
-        $(self).closest('.form-box').toggleClass('is-top', (evt.type === 'focus' || self.value.length > 0));
-      }
-    }, true);
+  function initLabels(labels) {
+    const targets = labels || document.querySelectorAll('.form-box input, .form-box textarea');
+    for (let i = 0; i < targets.length; i += 1) {
+      const item = targets[i];
+      item.addEventListener('animationstart', (evt) => {
+        const self = evt.target;
+        $(self).closest('.form-box').addClass('is-top');
+      }, true);
+      item.addEventListener('focus', (evt) => {
+        const self = evt.target;
+        if ($(self).siblings('input').length === 0) {
+          $(self).closest('.form-box').toggleClass('is-top', (evt.type === 'focus' || self.value.length > 0));
+        } else if ($(self).siblings('input')[0].value.length === 0) {
+          $(self).closest('.form-box').toggleClass('is-top', (evt.type === 'focus' || self.value.length > 0));
+        }
+      }, true);
+      item.addEventListener('blur', (evt) => {
+        const self = evt.target;
+        if ($(self).siblings('input').length === 0) {
+          $(self).closest('.form-box').toggleClass('is-top', (evt.type === 'focus' || self.value.length > 0));
+        } else if ($(self).siblings('input')[0].value.length === 0) {
+          $(self).closest('.form-box').toggleClass('is-top', (evt.type === 'focus' || self.value.length > 0));
+        }
+      }, true);
+      item.addEventListener('input', (evt) => {
+        const self = evt.target;
+        if ($(self).siblings('input').length === 0) {
+          $(self).closest('.form-box').toggleClass('is-top', (evt.type === 'focus' || self.value.length > 0));
+        } else if ($(self).siblings('input')[0].value.length === 0) {
+          $(self).closest('.form-box').toggleClass('is-top', (evt.type === 'focus' || self.value.length > 0));
+        }
+      }, true);
+    }
   }
+
+  initLabels();
+
+  window.setLabelListeners = initLabels;
 }
