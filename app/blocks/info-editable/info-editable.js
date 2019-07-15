affe3b47f9574c6b85c572857ab5b9b31336880b
Allkrain <aukra25@yandex.ru>
2019-07-15 17:39:47 +0300

    popup invoices

--- app/blocks/info-editable/info-editable.js
+++ app/blocks/info-editable/info-editable.js
@@ -1,4 +1,5 @@
 const $ = window.$;
+const bp = window.globalOptions.sizes;
 
 export default function infoEditable() {
   $(document).on('click', '.js-field-edit', (evt) => {
@@ -7,7 +8,9 @@ export default function infoEditable() {
     const editFieldData = $(self).attr('data-edit-target');
     const editField = $(`[data-field-edit="${editFieldData}"]`);
     $(editField).attr('contenteditable', 'true');
-    $(editField)[0].focus();
+    setTimeout(() => {
+      $(editField).focus();
+    }, 50);
   });
 
   $(document).on('keydown', '[data-field-edit]', (evt) => {
@@ -17,7 +20,9 @@ export default function infoEditable() {
     const copyfield = $(`[data-field-copy="${fieldData}"]`);
     const key = evt.which;
     if (key === 13) {
-      $(self).removeAttr('contenteditable');
+      if (window.Modernizr.mq(`(min-width: ${bp.md}px)`)) {
+        $(self).removeAttr('contenteditable');
+      }
       $(copyfield).val($(self).text());
     }
   });
@@ -34,6 +39,8 @@ export default function infoEditable() {
     const fieldData = $(self).attr('data-field-edit');
     const copyfield = $(`[data-field-copy="${fieldData}"]`);
     $(copyfield).val($(self).text());
-    $(self).removeAttr('contenteditable');
+    if (window.Modernizr.mq(`(min-width: ${bp.md}px)`)) {
+      $(self).removeAttr('contenteditable');
+    }
   });
 }
