7f82e233fa6c74ebb4ae92f171f05bb6d341e49c
Allkrain <aukra25@yandex.ru>
2019-08-13 19:55:01 +0300

    cropper

---
+++ app/blocks/photo-preview/photo-preview.js
@@ -0,0 +1,67 @@
+import Cropper from 'cropperjs';
+
+const $ = window.$;
+
+export default function photoPreview() {
+  function initCrop(starter) {
+    const preview = $(starter).closest('[data-preview-item]');
+    const inputName = $(preview).attr('data-name-crop-input');
+    const image = $(preview).find('[data-crop-image]');
+    const cropper = new Cropper(image[0], {
+      aspectRatio: (1064 / 596),
+      dragMode: 'move',
+      viewMode: 1,
+      autoCropArea: 1,
+      restore: true,
+      guides: false,
+      center: false,
+      responsive: true,
+      highlight: false,
+      cropBoxMovable: false,
+      cropBoxResizable: false,
+      background: false,
+      toggleDragModeOnDblclick: false,
+      crop() {
+        const img = this.cropper.getCroppedCanvas({
+          minWidth: 800,
+          maxWidth: 4096,
+          imageSmoothingEnabled: false,
+          imageSmoothingQuality: 'high',
+        });
+        const imgfinal = img.toDataURL('image/jpeg');
+        let input = $(preview).find('[data-file-crop]');
+        if ($(input).length > 0) {
+          $(input).val(imgfinal);
+        } else {
+          input = document.createElement('input');
+          input.setAttribute('type', 'hidden');
+          input.setAttribute('name', inputName);
+          input.setAttribute('data-file-crop', 'data-file-crop');
+          input.value = imgfinal;
+          $(preview).append(input);
+        }
+      },
+    });
+    preview[0].cropper = cropper;
+    $(preview).addClass('is-cropper-active');
+  }
+
+  function removeCropper(cropped) {
+    const input = $(cropped).find('[data-file-crop]');
+    cropped.cropper.destroy();
+    $(cropped).removeClass('is-cropper-active');
+    $(input).remove();
+  }
+
+  const starter = $('[data-crop-starter]:checked');
+  if (starter.length > 0) {
+    initCrop(starter);
+  }
+
+  $(document).on('change', '[data-crop-starter]', (evt) => {
+    const self = evt.currentTarget;
+    const lastCropped = $('.is-cropper-active')[0];
+    removeCropper(lastCropped);
+    initCrop(self);
+  });
+}
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -69,6 +69,7 @@ import infoNoitces from '../blocks/info-notices/info-notices';
 import compositeTab from '../blocks/composite-tab/composite-tab';
 import newSpaceStep6 from '../pages/new-space/new-space-step8';
 import newSpaceStep5 from '../pages/new-space/new-space-step5';
+import photoPreview from '../blocks/photo-preview/photo-preview';
 
 const $ = window.$;
 
@@ -127,4 +128,5 @@ $(() => {
   compositeTab();
   newSpaceStep6();
   newSpaceStep5();
+  photoPreview();
 });
