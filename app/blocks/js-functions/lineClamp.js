5c9cfd79999430f4e17d19aaa0bcc12f3b67e1bf
Allkrain <aukra25@yandex.ru>
2019-12-18 15:08:18 +0300

    500 and new offer select

--- app/blocks/js-functions/lineClamp.js
+++ app/blocks/js-functions/lineClamp.js
@@ -12,6 +12,19 @@ export default function truncateBlocks() {
     }
   }
 
+  function clamp2() {
+    const blocks = document.querySelectorAll('.offer-select__dropdown .js-2line');
+    if (blocks.length > 0) {
+      for (let i = 0; i < blocks.length; i += 1) {
+        const el = blocks[i];
+        const maxLines = 2;
+        webkitLineClamp(el, maxLines);
+      }
+    }
+  }
+
   clamp();
+  clamp2();
   window.addEventListener('resize', clamp, false);
+  window.addEventListener('resize', clamp2, false);
 }
