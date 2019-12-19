b3f1c6d1039bc61d73ee60ced08f47b9cc448563
Allkrain <aukra25@yandex.ru>
2019-12-19 13:03:59 +0300

    popup order summary g & h

--- app/blocks/info-editable/info-editable.js
+++ app/blocks/info-editable/info-editable.js
@@ -7,7 +7,7 @@ export default function infoEditable() {
     const self = evt.currentTarget;
     const editFieldData = $(self).attr('data-edit-target');
     const editField = $(`[data-field-edit="${editFieldData}"]`);
-    $(editField).attr('contenteditable', 'true');
+    $(editField).removeAttr('readonly');
     setTimeout(() => {
       $(editField).focus();
     }, 50);
@@ -16,31 +16,18 @@ export default function infoEditable() {
   $(document).on('keydown', '[data-field-edit]', (evt) => {
     evt.stopPropagation();
     const self = evt.currentTarget;
-    const fieldData = $(self).attr('data-field-edit');
-    const copyfield = $(`[data-field-copy="${fieldData}"]`);
     const key = evt.which;
     if (key === 13) {
       if (window.Modernizr.mq(`(min-width: ${bp.md}px)`)) {
-        $(self).removeAttr('contenteditable');
+        $(self).attr('readonly', 'readonly');
       }
-      $(copyfield).val($(self).text());
     }
-  });
-
-  $(document).on('change', '[data-field-edit]', (evt) => {
-    const self = evt.currentTarget;
-    const fieldData = $(self).attr('data-field-edit');
-    const copyfield = $(`[data-field-copy="${fieldData}"]`);
-    $(copyfield).val($(self).text());
   });
 
   $(document).on('blur', '[data-field-edit]', (evt) => {
     const self = evt.currentTarget;
-    const fieldData = $(self).attr('data-field-edit');
-    const copyfield = $(`[data-field-copy="${fieldData}"]`);
-    $(copyfield).val($(self).text());
     if (window.Modernizr.mq(`(min-width: ${bp.md}px)`)) {
-      $(self).removeAttr('contenteditable');
+      $(self).attr('readonly', 'readonly');
     }
   });
 }
