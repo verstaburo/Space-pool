61d9bf029f1a8e6766142c47d7f9f04276cd7758
Allkrain <aukra25@yandex.ru>
2020-03-24 17:05:04 +0300

    request-reservation popup

--- app/components/reservation-request/reservation-request.js
+++ app/components/reservation-request/reservation-request.js
@@ -16,6 +16,8 @@ export default function reservationRequestShow() {
   $(document).on('click', '[data-rr-need-to-show]', (evt) => {
     const self = evt.currentTarget;
     const isShow = $(self).attr('data-rr-need-to-show');
+    const isPopup = ($(self).attr('data-rr-popup') === 'true');
+    const isShowButton = ($(self).attr('data-rr-button-clone') === 'true');
     if (isShow === 'true') {
       evt.preventDefault();
       const wrapperName = $(self).attr('data-rr-wrapper-target');
@@ -24,6 +26,29 @@ export default function reservationRequestShow() {
       const form = $(`${rrId}`);
       $(parent).append(form);
       $(parent).addClass('is-show');
+      if (isPopup) {
+        window.globalFunctions.freeze();
+        $(parent).addClass('is-popup');
+        $('body').addClass('is-rr-popup-show');
+      }
+      if (isShowButton) {
+        const button = $(self)[0];
+        $(self).addClass('is-cloned');
+        let btnWrapper = $(button)[0];
+        if ($(button).closest('.offer-short__buttons').length > 0) {
+          btnWrapper = $(button).closest('.offer-short__buttons')[0];
+        }
+        const btnWrapParam = btnWrapper.getBoundingClientRect();
+        const btnWrapperClone = $(btnWrapper).clone();
+        $(btnWrapperClone).addClass('is-rr-clone');
+        $(btnWrapperClone).css({
+          height: btnWrapParam.height,
+          width: btnWrapParam.width,
+          top: btnWrapParam.top,
+          left: btnWrapParam.left,
+        });
+        $(parent).append(btnWrapperClone);
+      }
     }
   });
 
@@ -34,7 +59,15 @@ export default function reservationRequestShow() {
     const wrapper = $(self).closest('[data-rr-wrapper-block]');
     const form = $(self).closest('form');
     const nextStep = $(form).attr('data-first-step');
+    const isPopup = $(wrapper).is('.is-popup');
     $(wrapper).removeClass('is-show');
     window.stpFormGoToNextStep(form, nextStep, true);
+    if (isPopup) {
+      window.globalFunctions.unfreeze();
+      $(parent).removeClass('is-popup');
+      $('body').removeClass('is-rr-popup-show');
+    }
+    $('.is-cloned').removeClass('is-cloned');
+    $('.is-rr-clone').remove();
   });
 }
