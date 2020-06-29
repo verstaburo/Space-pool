3f29b8fed8a39eea71dcc122a62aba5e23942289
Allkrain <aukra25@yandex.ru>
2020-06-29 22:44:07 +0300

    search prefilter

--- app/blocks/form-elements/form-elements.js
+++ app/blocks/form-elements/form-elements.js
@@ -472,6 +472,52 @@ export function sliders() {
       inputs[handle].trigger('change');
     });
   });
+
+  // Параметры берутся из дата-атрибутов
+  $('.js-nd-range').each(function () {
+    const el = $(this);
+
+    const currency = el.data('currency') || '';
+    const margin = el.data('margin') || 0;
+    const step = el.data('step') || 1;
+    noUiSlider.create(el.find('[data-nd-range-container]').get(0), {
+      start: el.data('start'),
+      connect: el.data('connect'),
+      margin,
+      step,
+      range: {
+        min: el.data('min'),
+        max: el.data('max'),
+      },
+    });
+
+    const range = el.find('[data-nd-range-container]').get(0);
+    const output = el.find('[data-nd-range-output]');
+
+    if (output.length > 0) {
+      const nodes = [$(el).find('[data-nd-range-min]'), $(el).find('[data-nd-range-max]')];
+      range.noUiSlider.on('update', (values, handle) => {
+        nodes[handle].text(`${currency} ${parseInt(values[handle], 10)}`);
+      });
+    }
+
+    const inputs = [$(el).find('[data-nd-range-input-min]'), $(el).find('[data-nd-range-input-max]')];
+    range.noUiSlider.on('update', (values, handle) => {
+      inputs[handle].val(`${currency} ${parseInt(values[handle], 10)}`);
+      inputs[handle].trigger('change');
+    });
+
+    $(document).on('blur', '[data-nd-range-input-min], [data-nd-range-input-max]', (evt) => {
+      const self = evt.currentTarget;
+      const rng = $(self).closest('.js-nd-range');
+      const sldr = $(rng).find('[data-nd-range-container]').get(0);
+      const minInput = $(rng).find('[data-nd-range-input-min]');
+      const maxInput = $(rng).find('[data-nd-range-input-max]');
+      const minVal = +$(minInput).val().replace(/\D+/g, '') || 0;
+      const maxVal = +$(maxInput).val().replace(/\D+/g, '') || minVal;
+      sldr.noUiSlider.set([minVal, maxVal]);
+    });
+  });
 }
 
 $.fn.datepicker.language.en = {
