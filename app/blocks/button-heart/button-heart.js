c799337f99ff6c3feb5f27b770bd00082ecbc547
Allkrain <aukra25@yandex.ru>
2020-09-07 18:44:01 +0300

    hearts in search types and notifications

--- app/blocks/button-heart/button-heart.js
+++ app/blocks/button-heart/button-heart.js
@@ -3,6 +3,6 @@ const $ = window.$;
 export default function heartActivation() {
   $(document).on('click', '.js-heart', (evt) => {
     evt.preventDefault();
-    $(evt.target).toggleClass('is-active');
+    $(evt.currentTarget).toggleClass('is-active');
   });
 }
