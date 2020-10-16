e8d2b537a859bc2e00c864f7360a30d27c786637
Allkrain <aukra25@yandex.ru>
2020-10-16 20:17:51 +0300

    update from https://docs.google.com/document/d/1NRV_KqKucPGXMi9SztpROCV0DZd55o3-Poz9HHdVGSQ/edit - numbers #2,#3, #4 (in rent part), #8

--- app/blocks/tooltip/tooltip.js
+++ app/blocks/tooltip/tooltip.js
@@ -8,8 +8,13 @@ export default function tooltips() {
   $('.js-tooltip').tooltipster({
     animation: 'fade',
     delay: 0,
-    side: 'top',
-    theme: 'tooltipster-borderless',
+    side: ['top', 'right', 'bottom', 'left'],
+    trigger: 'click',
+    arrow: false,
+    distance: 0,
+    interactive: true,
+    maxWidth: 620,
+    theme: 'sp-border',
   });
 }
 /* eslint-enable no-unused-vars */
--- app/scripts/functions/switchFormContent.js
+++ app/scripts/functions/switchFormContent.js
@@ -1,8 +1,9 @@
 const $ = window.$;
 
 export default function switchFormContent() {
-  function hideAndShow(name) {
-    const newElements = $(`[data-form-switchable-item*="${name}"]`);
+  function hideAndShow(names) {
+    const selector = $(names).map((i, name) => `[data-form-switchable-item*="${name}"]`).get();
+    const newElements = $(selector.join(','));
     const newActiveElements = $(newElements).find('input, textarea, select');
     const allElements = $('[data-form-switchable-item]').not(newElements);
     const allActiveElements = $(allElements).find('input, textarea, select');
@@ -18,22 +19,35 @@ export default function switchFormContent() {
 
   function switchElements(sw) {
     const type = $(sw).is('select') ? 'select' : 'input';
+    const totalSwInputs = $('input[data-form-switch]:checked').not(sw);
+    const totalSwSelect = $('select[data-form-switch]').not(sw);
+    const names = [];
     switch (type) {
       case 'select': {
         const option = $(sw).find('option:selected');
         const obj = JSON.parse($(option).attr('data-custom-properties'));
         if (typeof obj === 'object') {
           const name = obj.formSwitch;
-          hideAndShow(name);
+          names.push(name);
         }
         break;
       }
       default: {
         const name = $(sw).attr('data-form-switch');
-        hideAndShow(name);
+        names.push(name);
         break;
       }
     }
+    $(totalSwInputs).each((i, input) => names.push(input.getAttribute('data-form-switch')));
+    $(totalSwSelect).each((i, select) => {
+      const option = $(select).find('option:selected');
+      const obj = JSON.parse(option.getAttribute('data-custom-properties'));
+      if (typeof obj === 'object') {
+        const name = obj.formSwitch;
+        names.push(name);
+      }
+    });
+    hideAndShow(names);
     if ($('[data-checker]').length > 0) {
       $('[data-checker]').not('[disabled]').each((i, checkers) => {
         const checker = checkers;
